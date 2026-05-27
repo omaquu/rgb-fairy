915c715cb7b60bb0215d0104f65e24b7fe81f987
Pina <pina@hermes.ai>
2026-05-27 18:26:32 +0000

    Add System.Windows.Input using for Cursors

--- FairyRgbController/MainWindow.xaml.cs
+++ FairyRgbController/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using FairyRgbController.Models;
 using FairyRgbController.Services;
